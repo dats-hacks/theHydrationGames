bdf2cdb050b5ca57d4a176b6146018ef86c4289e
DejonGill <dejonjgill@gmail.com>
2016-05-21 13:09:56 -0700

    Action for graph completed

--- actions/actionGraph.js
+++ actions/actionGraph.js
@@ -1,10 +1,20 @@
 import fetch from 'isomorphic-fetch';
 
-export const GET_DATASET = 'GET_DATASET';
+export const RECEIVE_DATA_SET = 'RECEIVE_DATA_SET';
 
 export function getDataSet () {
   return dispatch => {
     return fetch('/hello')
-    .then(response => response.)
-  }
+    .then(response => {
+      dispatch(receiveDataSet(response));
+    });
+  };
+}
+
+
+export function receiveDataSet(data) {
+  return {
+    type: RECEIVE_DATA_SET,
+    data
+  };
 }
